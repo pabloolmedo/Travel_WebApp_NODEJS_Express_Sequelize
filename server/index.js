f0c706f897448035ea1f2e0f1d8f3a1daa92ac88
Pablo Olmedo <pablo.olmedo@cba.gob.ar>
2020-08-05 19:28:17 +1000

    PUG view engine added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 //importar express
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
+
 
 //configurar express
 const app = express();
+
+//habilitar pug
+app.set('view engine', 'pug');
+
+//agregar vistas
+app.set('views', path.join(__dirname, './views'));
 
 //cargar las rutas
 app.use('/', routes());
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,11 @@ const router = express.Router();
 module.exports = function () {
 
     router.get('/', (req, res) => {
-        res.send('Inicio');
+        res.render('index');
     });
 
     router.get('/nosotros', (req, res) => {
-        res.send('Nosotros');
+        res.render('us');
     });
 
     return router;
